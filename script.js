6c0cce5cca7c60352727ae9f7e4ed0bfe921b286
Manish Prasad <100564494+manisprasad@users.noreply.github.com>
2023-12-02 15:48:44 +0530

    ui upgrade and more info added

--- script.js
+++ script.js
@@ -18,29 +18,134 @@ const movieName = async (movie) => {
 
 
 let allMovieName = document.querySelector(".all-movie-name");
+let cancel = document.querySelector(".cancel");
+let container = document.querySelector(".container");
+let moreInfoPopup = document.querySelector(".moreinfo-popup");
 let movieSearchName = document.querySelector("#movie-search");
 
+let moviesDetails;
 
-function createMovieDiv(name, image, rating) {
+function createMovieDiv(name, image, rating, link, language, genre,summary,premired,type,status) {
     let movieDiv = document.createElement("div");
     movieDiv.classList.add("movie");
 
-    let movieName = document.createElement("h2");
-    movieName.textContent = name;
+    let moviePoster = document.createElement("img");
+    moviePoster.src = image;
+    moviePoster.alt = `${name} Poster`;
 
-    let movieImage = document.createElement("img");
-    movieImage.src = image;
-    movieImage.alt = `${name} Poster`;
+    moviesDetails = document.createElement("div");
+    moviesDetails.classList.add("movie-details");
 
-    let movieRating = document.createElement("p");
-    movieRating.textContent = `Rating: ${rating}`;
+    let movieName = document.createElement("div");
+    movieName.classList.add("movie-div");
+    movieName.innerText = name;
 
-    movieDiv.appendChild(movieName);
-    movieDiv.appendChild(movieImage);
-    movieDiv.appendChild(movieRating);
+    let ratingDiv = document.createElement("div");
+    ratingDiv.classList.add("rating-div");
+    ratingDiv.innerText = `Rating: ${rating}`;
 
+    let languageDiv = document.createElement("div");
+    languageDiv.classList.add("language-div");
+    languageDiv.innerText = `Language: ${language}`;
+
+    let genreDiv = document.createElement("div");
+    genreDiv.classList.add("genre-div");
+    genreDiv.innerText = `Genre: ${genre}`;
+
+    // Append movieName, ratingDiv, languageDiv, and genreDiv to moviesDetails
+    moviesDetails.appendChild(movieName);
+    moviesDetails.appendChild(ratingDiv);
+    moviesDetails.appendChild(languageDiv);
+    moviesDetails.appendChild(genreDiv);
+
+    // Add a "More Info" button
+    let moreInfoButton = document.createElement("button");
+    moreInfoButton.classList.add("more-info-button");
+    moreInfoButton.innerText = "more Info...";
+
+    // Add a click event listener to the button
+    moreInfoButton.addEventListener("click", function() {
+        container.style.filter = "blur(8px)"
+        moreInfoPopup.innerHTML = ""
+       if ( moreInfoPopup.style.scale == "1") {
+        moreInfoPopup.style.scale = "0"
+        container.style.filter = "blur(0px)"
+
+       }else{
+        moreInfoPopup.style.scale = "1";
+        cancel.style.scale = "1"
+
+       }
+      let popupChild = createMovieDetailsDiv(name, summary,premired,type,status)
+        moreInfoPopup.appendChild(popupChild);
+
+        console.log(`More info clicked for ${name}`);
+    });
+
+    // Append the button to moviesDetails
+    moviesDetails.appendChild(moreInfoButton);
+
+    // Append the poster and moviesDetails to the movieDiv
+    movieDiv.appendChild(moviePoster);
+    movieDiv.appendChild(moviesDetails);
+
+    // Append the movieDiv to the container
     allMovieName.appendChild(movieDiv);
 }
+
+
+function createMovieDetailsDiv(name, summary, premiered, type, status) {
+    let mainDiv = document.createElement("div");
+    mainDiv.classList.add("more-details-info-div");
+
+    let titleDiv = document.createElement("div");
+    titleDiv.classList.add("title-div");
+    titleDiv.innerHTML = `<h1>${name}</h1>`
+
+    let summaryDiv = document.createElement("div");
+    summaryDiv.classList.add("summary-div");
+    summaryDiv.innerHTML = `overview: ${summary}`;
+
+    let premieredDiv = document.createElement("div");
+    premieredDiv.classList.add("premiered-div");
+    premieredDiv.innerText = `Premiered: ${premiered}`;
+
+    let typeDiv = document.createElement("div");
+    typeDiv.classList.add("type-div");
+    typeDiv.innerHTML = `Type: ${type}`;
+
+    let statusDiv = document.createElement("div");
+    statusDiv.classList.add("status-div");
+    statusDiv.innerHTML = `Status: ${status}`;
+
+    // Append titleDiv, summaryDiv, premieredDiv, typeDiv, and statusDiv to mainDiv
+    mainDiv.appendChild(titleDiv);
+    mainDiv.appendChild(summaryDiv);
+    mainDiv.appendChild(premieredDiv);
+    mainDiv.appendChild(typeDiv);
+    mainDiv.appendChild(statusDiv);
+
+    let closeButton = document.createElement("button")
+    closeButton.classList.add("cancel-button")
+    closeButton.innerText = "cancel";
+    cancel.appendChild(closeButton)
+
+    closeButton.addEventListener("click" , ()=>{
+        moreInfoPopup.style.scale = "0"
+        cancel.style.scale = "0"
+        container.style.filter = "blur(0px)"
+
+    })
+
+    return mainDiv;
+}
+
+// Example usage:
+
+
+
+
+
 let btn = document.querySelector(".btn");
 
 let loading = document.querySelector(".loader-container");
@@ -54,16 +159,40 @@ btn.addEventListener("click", () => {
             console.log(data);
 
             if (data.length === 0) {
-                allMovieName.innerHTML = `<h1>No movies related to your search </h1>`;
+                allMovieName.innerHTML = `<h1 style="color:white; text-align:center;">No movies related to your search </h1>`;
                 return;
             }
 
             data.forEach(ele => {
                 let name = ele.show.name;
-                let image = ele.show.image ? ele.show.image.medium : ''; // Check if image is available
-                let rating = ele.show.rating ? ele.show.rating.average : 'N/A'; // Check if rating is available
-                createMovieDiv(name, image, rating);
+                let summary = ele.show.summary;
+                let premeried = ele.show.premiered;
+                let type = ele.show.type;
+                let status = ele.show.status;
+                let language = ele.show.language;
+                let genre = ele.show.genres;
+                let image = ele.show.image ? ele.show.image.medium : '';
+                let officialSiteLink = ele.show.officialSite ? ele.show.officialSite : '#';
+                let rating = ele.show.rating ? ele.show.rating.average : 'N/A';
+                if (genre == "") {
+                    genre = "others"
+                }
+                if (rating == null) {
+                    rating = "N/A"
+                }
+                if (status === "Ended") {
+                    status = "<h2 style='color:red; display:inline-block;'>Ended</h2>";
+                }else if(status === "Running"){
+                    status = "<h2 style='color:green; display:inline-block;'>Running</h2>";
+                }else{
+                    status = `<h2 style='color:yellow; display:inline-block;'>${status}</h2>`;
+
+                }
+                
+                createMovieDiv(name, image, rating, officialSiteLink, language, genre,summary,premeried,type,status);
             });
+            // this.click() = officialSiteLink;
+            
 
         })
         .catch(error => {
@@ -79,24 +208,3 @@ movieSearchName.addEventListener("keydown", (event) => {
         btn.click();
     }
 });
-
-function createMovieDiv(name, image, rating) {
-    let movieDiv = document.createElement("div");
-    movieDiv.classList.add("movie");
-
-    let movieName = document.createElement("h2");
-    movieName.textContent = name;
-
-    let movieImage = document.createElement("img");
-    movieImage.src = image;
-    movieImage.alt = `${name} Poster`;
-
-    let movieRating = document.createElement("p");
-    movieRating.textContent = `Rating: ${rating}`;
-
-    movieDiv.appendChild(movieName);
-    movieDiv.appendChild(movieImage);
-    movieDiv.appendChild(movieRating);
-
-    allMovieName.appendChild(movieDiv);
-}
